7fc56c45261e57230258e0de28752c98ea764a93
Minod Nirmal <minodnirmal@gmail.com>
2023-12-05 15:53:06 +0530

    page module css

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import styles from './footer.module.css'; 
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>2023 Raveen Minod. All right reserved.</div>
       <div></div>
     </div>
