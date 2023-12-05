77337e0fdc232a77713e26a6616f31ba2b146a77
Minod Nirmal <minodnirmal@gmail.com>
2023-12-05 16:59:12 +0530

    navbar status

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+"use client"
 import Link from "next/link";
 import React from "react";
 import styles from './navbar.module.css';
@@ -38,13 +39,14 @@ const links = [
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <Link href="/">Raveen</Link>
-      <div>
+      <Link href="/" className={styles.logo}>Raveen</Link>
+      <div className={styles.links}>
         {links.map((link) => (
           <Link key={link.id} href={link.url}>
             {link.title}
           </Link>
         ))}
+        <button className={styles.logout} onClick={() => { console.log("Logout") }}>Logout</button>
       </div>
     </div>
   );
