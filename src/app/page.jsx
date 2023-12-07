c5e4e02bcf85868461cb6cbf1d12d4c9c95c4baf
RaveenMinod <minodnirmal@gmail.com>
2023-12-07 19:46:57 +0530

    footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <div>
-      Home
+      <Image width={100} height={100} src="https://images.pexels.com/photos/19384411/pexels-photo-19384411/free-photo-of-early-sunrise-at-mesa-arch.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt=" Testing Image"/>
     </div>
-  )
+  ) 
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import styles from './footer.module.css'; 
+import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>2023 Raveen Minod. All right reserved.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image src="/1.png" width={15} className={styles.icon} height={15} alt="Raveen minod" />
+        <Image src="/2.png" width={15} className={styles.icon} height={15} alt="Raveen minod" />
+        <Image src="/3.png" width={15} className={styles.icon} height={15} alt="Raveen minod" />
+        <Image src="/4.png" width={15} className={styles.icon} height={15} alt="Raveen minod" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
