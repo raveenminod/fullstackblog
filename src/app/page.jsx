80d732f6b30a3c29f8171ba688d319675515b5c2
Minod Nirmal <minodnirmal@gmail.com>
2023-12-08 08:34:47 +0530

    homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,23 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Hero from "public/hero.png";
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital products.</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quia
+          quasi laboriosam explicabo dolor vitae eos voluptate beatae ad ipsam
+          sapiente voluptatem ullam impedit nostrum, dolores est consequatur
+          alias velit?
+        </p>
+        <button className={styles.btn}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="" className={styles.img} />
+      </div>
     </div>
-  )
+  );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import styles from './footer.module.css'; 
+import styles from './footer.module.css';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>2023 Raveen Minod. All right reserved.</div>
-      <div></div>
+      <div className={styles.socialImg}>
+        <Image src="/1.png" width={15} height={15} className={styles.icon} alt='Social Icon' />
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt='Social Icon' />
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt='Social Icon' />
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt='Social Icon' />
+      </div>
     </div>
   )
 }
