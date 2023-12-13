5460f3f735a47ae7fa857bb613f0a9cca3d1b50a
Minod Nirmal <minodnirmal@gmail.com>
2023-12-13 17:04:25 +0530

    about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
-import styles from './page.module.css'; 
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/button/Button";
 
 const About = () => {
   return (
-    <div className={styles.container}>About</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/12756665/pexels-photo-12756665.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          fill={true}
+          alt=""
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.imgText}>
+        <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+        <h2 className={styles.imgDesc}>
+          Handcrafting award winning digital experiences
+        </h2>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Who Are We?</h1>
+          <p style={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, natus
+            ea facilis sit nostrum impedit labore accusantium rem recusandae
+            iusto. Delectus reprehenderit praesentium veniam eius. Enim fuga
+            rerum minima odio?
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, natus
+            ea facilis sit nostrum impedit.
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, natus
+            ea facilis sit nostrum impedit labore accusantium rem recusandae
+            iusto. Delectus reprehenderit praesentium veniam eius. Enim fuga
+            rerum minima odio?
+          </p>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>What We Do?</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            qui nam repellat consequuntur assumenda aperiam sint nobis minus
+            inventore temporibus nihil, nemo quo reprehenderit! Saepe totam
+            corrupti nemo placeat consequuntur.
+          </p>
+          <Button url="/contact" text="Contact" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "public/hero.png";
+import Button from "@/components/button/Button";
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
           sapiente voluptatem ullam impedit nostrum, dolores est consequatur
           alias velit?
         </p>
-        <button className={styles.btn}>See Our Works</button>
+        <Button url="/portfolio" text="See Our Works" />
       </div>
       <div className={styles.item}>
         <Image src={Hero} alt="" className={styles.img} />
