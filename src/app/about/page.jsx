2dc1a69ce139bbfc41099ec0f7b6da24b84a3962
RaveenMinod <minodnirmal@gmail.com>
2023-12-13 22:13:03 +0530

    about button

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -13,24 +13,26 @@ const About = () => {
           alt=""
           className={styles.img}
         />
-      </div>
-      <div className={styles.imgText}>
-        <h1 className={styles.imgTitle}>Digital Storytellers</h1>
-        <h2 className={styles.imgDesc}>
-          Handcrafting award winning digital experiences
-        </h2>
+        <div className={styles.imgText}>
+          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+          <h2 className={styles.imgDesc}>
+            Handcrafting award winning digital experiences
+          </h2>
+        </div>
       </div>
       <div className={styles.textContainer}>
         <div className={styles.item}>
           <h1 className={styles.title}>Who Are We?</h1>
-          <p style={styles.desc}>
+          <p className={styles.desc}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, natus
             ea facilis sit nostrum impedit labore accusantium rem recusandae
             iusto. Delectus reprehenderit praesentium veniam eius. Enim fuga
             rerum minima odio?
             <br />
+            <br />
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, natus
             ea facilis sit nostrum impedit.
+            <br />
             <br />
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, natus
             ea facilis sit nostrum impedit labore accusantium rem recusandae
@@ -40,11 +42,15 @@ const About = () => {
         </div>
         <div className={styles.item}>
           <h1 className={styles.title}>What We Do?</h1>
-          <p>
+          <p className={styles.desc}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
             qui nam repellat consequuntur assumenda aperiam sint nobis minus
             inventore temporibus nihil, nemo quo reprehenderit! Saepe totam
             corrupti nemo placeat consequuntur.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            qui nam repellat
           </p>
           <Button url="/contact" text="Contact" />
         </div>
