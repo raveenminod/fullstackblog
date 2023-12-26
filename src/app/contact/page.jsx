0800e070fc598b843d649ad839f3960fdbf0eb68
RaveenMinod <minodnirmal@gmail.com>
2023-12-26 16:31:06 +0530

    keyframe

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import styles from './page.module.css'; 
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/button/Button";
 
 const Contact = () => {
   return (
-    <div className={styles.container}>Contact</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            fill={true}
+            alt=""
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea className={styles.textArea} cols="30" rows="10" placeholder="message"/>
+          <Button text="Send" url="#" />
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
