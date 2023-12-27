6c5b718aa6f6aa1cda37d0e1f0d60cff0c6c458c
RaveenMinod <minodnirmal@gmail.com>
2023-12-27 22:21:42 +0530

    portfolio 2

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Button from "@/components/button/Button";
+import Image from "next/image";
 
-const Category = () => {
+const Category = ({ params }) => {
   return (
-    <div>Category</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.contain}>
+          <h1 className={styles.title}>Testing</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil
+            repellendus maxime excepturi hic quae fuga dolor. Eum aliquam
+            dolorem quas tenetur deleniti quos, omnis at. Officiis quidem
+            quaerat sapiente doloremque.
+          </p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            fill={true}
+            className={styles.img}
+            src="https://images.pexels.com/photos/7790175/pexels-photo-7790175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Category
+export default Category;
