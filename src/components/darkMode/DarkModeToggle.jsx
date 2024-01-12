1fef07f3c272c2982f03d0baa9728526f2937ea9
Minod Nirmal <minodnirmal@gmail.com>
2024-01-12 17:11:16 +0530

    dark mode v3

--- src/components/darkMode/DarkModeToggle.jsx
+++ src/components/darkMode/DarkModeToggle.jsx
@@ -6,18 +6,18 @@ import { ThemeContext } from "@/context/ThemeContext";
 
 const DarkModeToggle = () => {
 
-    const {toggle, mode} = useContext(ThemeContext);
+    const { toggle, mode } = useContext(ThemeContext);
 
-  return (
-    <div className={styles.container} onClick={toggle}>
-      <div className={styles.icon}>☀</div>
-      <div className={styles.icon}>☁</div>
-      <div
-        className={styles.ball}
-        style={mode === "light" ? { left: "2px" } : { right: "2px" }}
-      />
-    </div>
-  );
+    return (
+        <div className={styles.container} onClick={toggle}>
+            <div className={styles.icon}>☀</div>
+            <div className={styles.icon}>☁</div>
+            <div
+                className={styles.ball}
+                style={mode === "light" ? { left: "2px" } : { right: "2px" }}
+            />
+        </div>
+    );
 };
 
 export default DarkModeToggle;
