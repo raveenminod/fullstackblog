55d2dadff9c26cdde968088c0855d33b91944354
Minod Nirmal <minodnirmal@gmail.com>
2024-01-18 16:48:39 +0530

    blog

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
 
-const BlogPost = () => {
+const BlogPost = ({ params }) => {
   return (
-    <div>BlogPost</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>Desc</p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/13798633/pexels-photo-13798633.jpeg"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Raveen Minod</span>
+          </div>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/5687494/pexels-photo-5687494.jpeg"
+            alt=""
+            width={40}
+            height={40}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+          maxime ea, accusamus quam cupiditate dolorum modi sed vel nisi
+          perspiciatis consequatur magnam quasi ad explicabo ipsa amet saepe
+          tempora omnis.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default BlogPost
+export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,31 +3,44 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-const Blog = () => {
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/", {
+    next: { revalidate: 10 },
+  });
+  //data dynamicaly changing use {cache: 'no-store}
+  // The return value is not serialized
+  // You can return Date, Map, Set, etc.
+
+  //Hanndle error
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const Blog = async () => {
+  const data = await getData();
+
   return (
     <div className={styles.container}>
-      <Link href="/testId" className={styles.section}>
-        <div className={styles.imgContainer}>
-          <Image
-            className={styles.img}
-            width={400}
-            height={250}
-            src="https://images.pexels.com/photos/7790175/pexels-photo-7790175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          </h1>
-          <p className={styles.desc}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam quod
-            illo doloribus rerum! Beatae explicabo animi eum nulla inventore
-            dolor rem error sunt odio suscipit, modi sint. Natus, praesentium
-            nihil.
-          </p>
-        </div>
-      </Link>
+      {data.map((item) => (
+        <Link href="/blog/testId" className={styles.section} key={item.id}>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              width={400}
+              height={250}
+              src="https://images.pexels.com/photos/7790175/pexels-photo-7790175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.body}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
